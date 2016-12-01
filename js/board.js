239e6619ed1a4246f2d88673a8f7cdc3477f1c37
Ronald Manganaro <ronaldmanganaro@gmail.com>
2016-12-01 15:20:34 -0500

    built the board

---
+++ js/board.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    img = "<img src='images/board/tiles_empty.png' />";
+    $('td.tiles_empty').append(img);
+
+    img = "<img src='images/board/tiles_dl.png' />";
+    $('td.tiles_dl').append(img);
+
+    img = "<img src='images/board/tiles_dw.png' />";
+    $('td.tiles_dw').append(img);
+
+    img = "<img src='images/board/tiles_tl.png' />";
+    $('td.tiles_tl').append(img);
+
+    img = "<img src='images/board/tiles_tw.png' />";
+    $('td.tiles_tw').append(img);
+
+    img = "<img src='images/board/tiles_start.png' />";
+    $('td.tiles_start').append(img);
+
+    img = "<img src='images/board/tiles_empty_alt.png' />";
+    $('td.tiles_empty_alt').append(img);
+
+
+});
+
