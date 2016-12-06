55fa598c68a06a5541aafde9017141b79c565046
Ronald Manganaro <ronaldmanganaro@gmail.com>
2016-12-06 17:47:07 -0500

    done with mandatory

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-var ScrabbleTiles = [];
+var ScrabbleTiles = []; // Global variable used to hold the tiles
 ScrabbleTiles[0] = { "letter": "a", "value": 1, "original_distribution": 9, "number_remaining": 9 };
 ScrabbleTiles[1] = { "letter": "b", "value": 3, "original_distribution": 2, "number_remaining": 2 };
 ScrabbleTiles[2] = { "letter": "c", "value": 3, "original_distribution": 2, "number_remaining": 2 };
@@ -27,8 +27,10 @@ ScrabbleTiles[24] = { "letter": "y", "value": 4, "original_distribution": 2, "nu
 ScrabbleTiles[25] = { "letter": "z", "value": 10, "original_distribution": 1, "number_remaining": 1 };
 ScrabbleTiles[26] = { "letter": "", "value": 0, "original_distribution": 2, "number_remaining": 2 };
 
-var turn = 1;
+var turn = 1; // Used to tell if just started and needs 7 tiles
+
 $(document).ready(function () {
+    //setup the board then create peieces for the player
     setupBoard();
     selectPieces();
 });
@@ -38,6 +40,7 @@ function selectPieces() {
     var tilesNeeded = 7;
     var handPos;
 
+    // gives the player 7 tiles
     for (i = 0; tilesNeeded > 0; i++) {
         //generate random number to get random tile
         var randomNum = Math.floor(Math.random() * 27);
@@ -52,13 +55,13 @@ function selectPieces() {
         var tilename =
             "<img src='images/tiles/letter_" + ScrabbleTiles[randomNum].letter + ".png' class='piece' />";
 
-        //get create td to look for
+        // get correct position in players hand 
         handPos = "td#" + tmp;
 
-        //add img to div
+        // add img to td
         $(handPos).append(tilename);
 
-        //change handpos to look for div img
+        // change handpos to look for div img
         handPos += " img";
 
         //add position on rack to the piece on the rack
@@ -71,7 +74,6 @@ function selectPieces() {
             snapMode: "both",
             snapTolerance: 50,
             drag: function (event, ui) {
-
             }
         });
 
@@ -83,38 +85,43 @@ function selectPieces() {
 }
 
 function setupBoard() {
-
+    // add class boardtile to each td in the board table
     $('div#Board table tr td').addClass("boardTile");
     $('div#Board table tr td').attr('id', function (i) {
+        // add id based on position in the board
         return ++i;
     });
 
-
+    // boardtiles can have the players pieces dropped on them
     $(".boardTile").droppable({
         accept: '.piece',
         drop: function (event, ui) {
+            // finds out what piece was dropped so can add class onboard
             var posOnBoard = parseInt($(this).attr('id'));
-            //console.log(posOnBoard);
-
             $(ui.draggable).addClass('onboard');
             $(ui.draggable).attr("id", posOnBoard);
 
+            // find out which rack is empty now
             var string = $(ui.draggable).attr("class");
+            // part of classname is position piece was on rack
             var rackpos = parseInt(string.substring(6, 9));
             console.log("add empty to rack pos: " + rackpos);
             $('#' + rackpos).addClass('empty');
         }
     });
 
+    // rack takes back peices
     $(".rack").droppable({
         accept: '.piece',
         drop: function (event, ui) {
+            // when put back on rack take away classname onboard
             $(ui.draggable).removeClass('onboard');
             $(this).removeClass('empty');
         }
 
     });
 
+    // add any other boardtiles
     img = "<img src='images/board/tiles_empty.png' />";
     $('td.tiles_empty').append(img);
 
@@ -136,36 +143,26 @@ function setupBoard() {
     img = "<img src='images/board/tiles_empty_alt.png' />";
     $('td.tiles_empty_alt').append(img);
 
-
+    // button that checks if word is in a valid position
     $('button').click(checkPosition);
 }
 
 function checkPosition() {
-    //check if connected
-    var onboardPieces = $(".onboard");
-    var size = $(".onboard").size();
 
-    var posOk = true;
-    var posArr = [];
-    //sort least to greatest
+    var onboardPieces = $(".onboard"); // array of pieces on board
+    var size = $(".onboard").size(); // how many peices were places
+    var posOk = true; // if all the places pieces are connected
+    var posArr = []; // will store the position of each peice
 
-
-    //check right of the first element
-    //if theres an item to right keep checking right for others
-    //else check down if yes keep checking down 
-    //else not in right pos
-    //not needed anymore
-    var atStart = true;
-
+    // storing locations of pieces in array
     for (j = 0; j < size; j++) {
-        if (turn === 1 && (parseInt(onboardPieces[j].id) === 8)) {
-            atStart = true
-        }
         posArr.push(parseInt(onboardPieces[j].id));
-
     }
+
+    // sort them least to greatest by location
     posArr.sort(sortArr);
-    //console.log(posArr);
+
+    // check that ids increase their position by one
     var length = posArr.length;
     length--;
     for (i = 0; i < length; i++) {
@@ -174,43 +171,29 @@ function checkPosition() {
         var next = posArr[num];
         var prevPlusOne = posArr[i];
         prevPlusOne++;
-        //console.log(posArr);
-        //console.log("next :" + next);
-        //console.log("prev :" + prevPlusOne);
-
 
         if (next === prevPlusOne) {
             posOk = true;
-
         } else {
+            // if even one tile is misplaced invalid positions
             posOk = false;
-            console.log("not connected");
             break;
         }
-
     }
 
-
-    console.log("Position was ok? " + posOk);
-    if (turn === 1 && !atStart) {
-        posOk = false;
-        console.log("actually it aint ok first turn need element at start");
-    }
-
+    // if they are placed right check if its a word
     if (posOk) {
         checkWord();
-
     }
 }
 
-
+// sorts numbers least to greatest
 function sortArr(a, b) {
-
     return (a - b);
 }
 
+// sorts ids least to greatest
 function sortById(a, b) {
-
     return (a.id - b.id);
 }
 
@@ -220,52 +203,60 @@ function checkWord() {
     var str = "";
     var srcArr = $(".onboard");
 
-    console.log(srcArr.sort(sortById));
-
     for (var k = 0; k < size; k++) {
-        console.log(srcArr[k].id);
         str = srcArr[k].src;
-
         var n = str.lastIndexOf('_');
         var result = str.substring(n + 1, n + 2);
+
+        // if its a blank tile replace . retrieved with space
         if (result === '.') {
-            //could ask player for letter they want to use
             result = ' ';
         }
         console.log(result);
         word += (result);
-
     }
-    console.log("the word is: " + word);
     lookup(word);
-    //update score
 }
 
 function lookup(word) {
+
+    $("img.onboard").draggable('disable');
+    calculateScore(word);
+    turn++;
+    newHand();
+    
+    /* ALL THIS CODE ONLY WORKS ON MY LOCAL MACHINE 
     var apikey = "6574d3d5-aa7a-4a8a-9063-e312b096015f";
     var xmlRequest = "https://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=" + apikey;
-    var xhttp = new XMLHttpRequest();
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + xmlRequest + '"') + '&format=xml&callback=?';
 
-/*
-        $.ajax({
-        type: "GET",
-        url: xmlRequest,
-        data:{q:idiom},
-        async:true,
-        dataType : 'jsonp',   //you may use jsonp for cross origin request
-        crossDomain:true,
-        success: function(data, status, xhr) {
-            alert(xhr.response);
+    // Request that YSQL string, and run a callback function.
+    // Pass a defined function to prevent cache-busting.
+    $.getJSON(yql, function (data) {
+
+        // the code you're looking for
+        var found = false;
+        var results = [];
+        var searchField = "suggestion";
+        var searchVal = "my Name";
+        // iterate over each element in the array
+        for (var i = 0; i < data.list.length; i++) {
+            if (data.list[i][searchField] === "") {
+                results.push(obj.list[i]);
+                found = true;
+            } else {
+                console.log("it is a word!");
+                $("img.onboard").draggable('disable');
+                calculateScore(word);
+                turn++;
+                newHand();
+            }
         }
     });
-*/
 
-
-    xhttp.onload = function () {
-        if (xhttp.status == 200) {
-            // Action to be performed when the document is read;
-            //console.log(xhttp.response);
-
+    /*
+     xhttp.onload = function () {
+         if (xhttp.status == 200) {
             var xml = xhttp.response,
                 xmlDoc = $.parseXML(xml),
                 $xml = $(xmlDoc),
@@ -279,18 +270,20 @@ function lookup(word) {
                 turn++;
                 newHand();
             }
-
-        } else
-            console.log("error api call");
-    };
-    xhttp.open("GET", xmlRequest, true);
-    xhttp.send(null);
+        }
+        
+             
+         } else
+             console.log("error api call");
+     };
+     xhttp.open("GET", xmlRequest, true);
+     xhttp.send(null);
+     */
 }
 
 var totalScore = 0;
 
 function calculateScore(word) {
-
     var onboardPieces = $(".onboard");
     var posArr = [];
     var size = $(".onboard").size();
@@ -307,16 +300,12 @@ function calculateScore(word) {
             var dw = true;
         } else if (posArr[j] === 4) {
             var dlAt4 = j;
-
         } else if (posArr[j] === 12) {
             var dlAt12 = j;
-
         }
-
     }
 
     var score = 0;
-
     for (i = 0; i < word.length; i++) {
         var character = word[i];
         var value = 0;
